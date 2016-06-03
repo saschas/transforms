00b83fa091a834d218a7b6593ceadbd8cc305f71
saschas <sascha.sigl@googlemail.com>
2016-06-03 20:01:40 +0200

    test app

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var fs = require('fs');
+		path = require('path');
+var app = express();
+		http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+//Start Connection
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  //chat event
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+
+    io.emit('chat message', msg);
+  });
+
+
+  //disconnect event
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+
+});
+
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
